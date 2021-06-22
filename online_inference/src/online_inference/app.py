067a57840192a0a20ce46af59b7ba678fe161592
sickMilk <51321815+sickMilk@users.noreply.github.com>
2021-06-23 02:01:57 +0300

    Docker part added

--- online_inference/src/online_inference/app.py
+++ online_inference/src/online_inference/app.py
@@ -54,4 +54,4 @@ def test(request: ModelOut):
     return request
 
 if __name__ == '__main__':
-    uvicorn.run('app:app', host='127.0.0.1', port=os.getenv('PORT', 8000))
+    uvicorn.run('app:app', host='0.0.0.0', port=os.getenv('PORT', 8000))
